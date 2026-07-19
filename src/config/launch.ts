2789167b0a5b9ec85b02c7c2dfafe047175696aa
JunHoLEE <junho357818@gmail.com>
2026-07-19 16:50:48 +0900

    Refresh information pages and DM contact flow

--- src/config/launch.ts
+++ src/config/launch.ts
@@ -1,3 +1,5 @@
 export const SHOW_REVIEWS = false;
 export const SHOW_BLOG_IN_NAV = false;
 export const TIKTOK_URL = 'https://www.tiktok.com/@roadieai';
+export const TELEGRAM_DM_URL = 'https://t.me/MyanMate?direct';
+export const MESSENGER_URL = 'https://m.me/61580519695929';
